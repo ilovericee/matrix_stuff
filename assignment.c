dafe2a08f24a0a1a7abefb27a6ecc31855063ffb
ilovericee <169096572+ilovericee@users.noreply.github.com>
2025-04-06 23:13:19 +0300

    some improvements

--- assignment.c
+++ assignment.c
@@ -1,3 +1,4 @@
+#include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
@@ -8,12 +9,6 @@
 #define ROWS2 3
 #define COLS2 2
 
-#if (ROWS1 == COLS2)
-int target[ROWS1][COLS2]; // for multiplication
-#else
-printf("Can't multiply quitting...\n");return 1;
-#endif 
-
 int array1[ROWS1][COLS1];
 int array2[ROWS2][COLS2];
 
@@ -22,34 +17,47 @@ int array2[ROWS2][COLS2];
 int did_print = 0;
 
 void randomValueGenerator(int a[ROWS1][COLS1],int b[ROWS2][COLS2]);
-void printMat(int a[ROWS1][COLS1],int b[ROWS2][COLS2],int target[ROWS1][COLS2]);
+void printMat(size_t row,size_t col,int matrix[row][col]);
 void matSum(int a[ROWS1][COLS1],int b[ROWS2][COLS2],int target[ROWS1][COLS2]);
 void matMul(int a[ROWS1][COLS1],int b[ROWS2][COLS2],int target[ROWS1][COLS2]);
 void neutralizeTarget(int target[ROWS1][COLS2]);
 
 int main()
 {
+  if(!(COLS1 == ROWS2))
+  {
+    printf("Can't multiply...\n");return 1;
+  }
+
+  int target[ROWS1][COLS2]; // for multiplication
+
   srand(time(NULL));
   randomValueGenerator(array1,array2);
   neutralizeTarget(target);
-  printMat(array1,array2,target);
-
+    
+  printf("-----------*Matrix-1*-----------\n");
+  printMat(ROWS1, COLS1, array1);
+  printf("-----------*Matrix-2*-----------\n");
+  printMat(ROWS2, COLS2, array2);
     
   // SUM
   if(ROWS1 != ROWS2 || COLS1 != COLS2)
   {
-    printf("--------------------------------------------------\nMatrix sizes are not the same so we can't add them\n--------------------------------------------------\n");
+     printf("--------------------------------------------------\n");
+     printf("Matrix sizes are not the same, so we can't add them\n");
+     printf("--------------------------------------------------\n");
   }
   else
   {
+    neutralizeTarget(target);
     matSum(array1,array2,target);
-    printMat(array1,array2,target);
+    printMat(ROWS1,COLS2,target);
   }
   // MULTIPLICATION
-  if(ROWS1 == COLS2){
+  if(COLS1 == ROWS2){
   neutralizeTarget(target);
   matMul(array1,array2,target);
-  printMat(array1,array2,target);
+  printMat(ROWS1,COLS2,target);
   }
   else
   {
@@ -77,40 +85,15 @@ void randomValueGenerator(int a[ROWS1][COLS1],int b[ROWS2][COLS2])
 
 }
 
-void printMat(int a[ROWS1][COLS1],int b[ROWS2][COLS2],int target[ROWS1][COLS2])
+void printMat(size_t row,size_t col,int matrix[row][col])
 {
-  if(did_print == 0){
-  printf("-----------*Matrix-1*-----------\n");
-  for(size_t i = 0;i<ROWS1;i++)
+  for(size_t i = 0;i<row;i++)
   {
-    for(size_t j = 0;j<COLS1;j++)
+    for(size_t j = 0;j<col;j++)
     {
-      printf("%d ",a[i][j]);
+      printf("%d ",matrix[i][j]);
     }
     printf("\n");
-  }
-  printf("-----------*Matrix-2*-----------\n");
-  for(size_t i = 0;i<ROWS2;i++)
-  {
-    for(size_t j = 0;j<COLS2;j++)
-    {
-      printf("%d ",b[i][j]);
-    }
-    printf("\n");
-  }
-    did_print = 1;
-  }
-  
-  if(target[0][0] != 0)
-  {
-     for(size_t i = 0;i<ROWS1;i++)
-  {
-    for(size_t j = 0;j<COLS2;j++)
-    {
-      printf("%d ",target[i][j]);
-    }
-    printf("\n");
-    }
   }
 }
 
